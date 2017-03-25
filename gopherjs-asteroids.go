7f4f7a6bbbd0a29bd85c45068671012f623576c0
Mike Janger <inchingforward@gmail.com>
2017-03-24 19:00:30 -0500

    Added ship.

--- gopherjs-asteroids.go
+++ gopherjs-asteroids.go
@@ -1,17 +1,43 @@
 package main
 
 import (
-	"fmt"
-
 	"honnef.co/go/js/dom"
 )
 
+var (
+	ship   = Ship{}
+	canvas *dom.HTMLCanvasElement
+	ctx    *dom.CanvasRenderingContext2D
+)
+
+// A Ship is the player.
+type Ship struct {
+	x int
+	y int
+}
+
+func (ship Ship) draw(ctx *dom.CanvasRenderingContext2D) {
+	ctx.StrokeStyle = "black"
+	ctx.LineWidth = 1
+	ctx.Translate(ship.x, ship.y)
+	ctx.MoveTo(0, -1)
+	ctx.MoveTo(0, -10)
+	ctx.LineTo(-7, 10)
+	ctx.LineTo(0, 8)
+	ctx.LineTo(7, 10)
+	ctx.LineTo(0, -10)
+	ctx.Stroke()
+}
+
 func update() {
-	fmt.Println("update")
+
 }
 
 func draw() {
-	fmt.Println("draw")
+	ctx.FillStyle = "white"
+	ctx.FillRect(0, 0, canvas.Width, canvas.Height)
+
+	ship.draw(ctx)
 }
 
 func gameLoop() {
@@ -21,5 +47,12 @@ func gameLoop() {
 }
 
 func main() {
+	doc := dom.GetWindow().Document()
+	canvas = doc.GetElementByID("canvas").(*dom.HTMLCanvasElement)
+	ctx = canvas.GetContext2d()
+
+	ship.x = canvas.Width / 2
+	ship.y = canvas.Height / 2
+
 	gameLoop()
 }
